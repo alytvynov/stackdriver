6141e79a934ba7f16d3599eb21ebb53fcb48b4fa
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2016-05-05 11:59:26 -0700

    Initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stackdriver client for [go-metrics](https://github.com/rcrowley/go-metrics)
+
+This is a simple client for go-metrics that submits all stats to Stackdriver.
---
+++ stackdriver.go
@@ -0,0 +1,54 @@
+package signalfx
+
+import (
+	"log"
+	"time"
+
+	"github.com/AMeng/stackdriver"
+	"github.com/rcrowley/go-metrics"
+)
+
+type Config struct {
+	APIKey     string
+	Prefix     string
+	InstanceID string
+	Log        *log.Logger
+}
+
+func Send(r metrics.Registry, d time.Duration, config Config) {
+	client := stackdriver.NewStackdriverClient(config.APIKey)
+	for range time.Tick(d) {
+		if err := send(r, client, config); err != nil {
+			config.Log.Println("stackdriver:", err)
+		}
+	}
+}
+
+func send(r metrics.Registry, client *stackdriver.StackdriverClient, config Config) error {
+	m := stackdriver.NewGatewayMessage()
+	fillMetrics(r, &m, config)
+	return client.Send(m)
+}
+
+func fillMetrics(r metrics.Registry, msg *stackdriver.GatewayMessage, config Config) {
+	now := time.Now().Unix()
+	r.Each(func(name string, i interface{}) {
+		if config.Prefix != "" {
+			name = config.Prefix + "." + name
+		}
+		switch m := i.(type) {
+		case metrics.Counter:
+			msg.CustomMetric(name, config.InstanceID, now, m.Count())
+		case metrics.Gauge:
+			msg.CustomMetric(name, config.InstanceID, now, m.Value())
+		case metrics.GaugeFloat64:
+			msg.CustomMetric(name, config.InstanceID, now, m.Value())
+		case metrics.Histogram:
+			msg.CustomMetric(name, config.InstanceID, now, m.Mean())
+		case metrics.Meter:
+			msg.CustomMetric(name, config.InstanceID, now, m.Rate1())
+		case metrics.Timer:
+			msg.CustomMetric(name, config.InstanceID, now, m.Mean())
+		}
+	})
+}
